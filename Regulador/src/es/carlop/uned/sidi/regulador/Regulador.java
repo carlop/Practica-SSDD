875550ac911da84da606b30db4807138de7775b4
Carlos López <yo@carlop.es>
2015-11-18 19:59:24 +0100

    Primer commit

---
+++ Regulador/src/es/carlop/uned/sidi/regulador/Regulador.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Práctica de Sistemas Distribuidos
+ * Curso 2015-2016 
+ * Alumno: Francisco Carlos López Porcel
+ * Correo electrónico: yo@carlop.es
+ *
+ */
+package es.carlop.uned.sidi.regulador;
+
+public class Regulador {
+
+    public static void main(String[] args) {
+        System.out.println("Regulador");
+    }
+
+}
