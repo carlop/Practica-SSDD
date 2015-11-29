3101ca792cb6ac01ac9a3bd6c0d4efd6c1f3a905
Carlos López <yo@carlop.es>
2015-11-29 20:36:34 +0100

    Añadidas las interfaces de los servicios de autenticación, mercancias y venta.

---
+++ Comun/src/es/carlop/uned/ssdd/comun/ServicioVentaInterface.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * Práctica de Sistemas Distribuidos
+ * Curso 2015-2016 
+ * Alumno: Francisco Carlos López Porcel
+ * Correo electrónico: yo@carlop.es
+ *
+ */
+package es.carlop.uned.ssdd.comun;
+
+public interface ServicioVentaInterface {
+
+}
