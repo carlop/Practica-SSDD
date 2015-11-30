5d6c17a551e98a80bc58ea18ee7d522c55b879a9
Carlos López <yo@carlop.es>
2015-11-30 23:23:47 +0100

    Nueva clase para generar los menus

---
+++ Comun/src/es/carlop/uned/ssdd/comun/InterfazGraficaUsuario.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * Práctica de Sistemas Distribuidos
+ * Curso 2015-2016 
+ * Alumno: Francisco Carlos López Porcel
+ * Correo electrónico: yo@carlop.es
+ *
+ */
+package es.carlop.uned.ssdd.comun;
+
+public class InterfazGraficaUsuario {
+    public static void generaMenu(String[] opciones) {
+	for (int i = 0; i < opciones.length; i++) {
+	   System.out.println((i+1) + ". " + opciones[i]); 
+	}
+	System.out.println(opciones.length + 1 + ". Salir");
+    }
+}
