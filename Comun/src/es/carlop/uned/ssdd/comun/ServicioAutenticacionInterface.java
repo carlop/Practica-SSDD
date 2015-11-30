868ae04cd53d3a1f762c1a23e18e6909b627760f
Carlos López <yo@carlop.es>
2015-11-30 22:45:52 +0100

    Relletada la intervaz del servicio de autenticacion con los posibles métodos. Añadido un enum nuevo para distinguir los clientes y los distribuidores.

--- Comun/src/es/carlop/uned/ssdd/comun/ServicioAutenticacionInterface.java
+++ Comun/src/es/carlop/uned/ssdd/comun/ServicioAutenticacionInterface.java
@@ -8,6 +8,12 @@
  */
 package es.carlop.uned.ssdd.comun;
 
-public interface ServicioAutenticacionInterface {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
+public interface ServicioAutenticacionInterface extends Remote {
+    public int registrar(String usuario, String password, TipoUsuario tipoUsuario) throws RemoteException;
+    public int autenticar(String usuario, String password) throws RemoteException;
+    public boolean salir(int id) throws RemoteException;
+    public boolean baja(int id) throws RemoteException;
 }
