d181fcd8a4806cd70faa4fa9f9845d1ce25e1849
Carlos López <yo@carlop.es>
2015-11-30 23:17:54 +0100

    Añadidos comentarios a la interfaz del servicio de autenticacion y un par de métodos para listar los clientes y los distribuidores.

--- Comun/src/es/carlop/uned/ssdd/comun/ServicioAutenticacionInterface.java
+++ Comun/src/es/carlop/uned/ssdd/comun/ServicioAutenticacionInterface.java
@@ -10,10 +10,19 @@ package es.carlop.uned.ssdd.comun;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 public interface ServicioAutenticacionInterface extends Remote {
+    // Registra un usuario nuevo en el sistema
     public int registrar(String usuario, String password, TipoUsuario tipoUsuario) throws RemoteException;
-    public int autenticar(String usuario, String password) throws RemoteException;
+    // Autentica un usuario en el sistema
+    public int autenticar(String usuario, String password, TipoUsuario tipoUsuario) throws RemoteException;
+    // Saca a un usuario del sistema
     public boolean salir(int id) throws RemoteException;
+    // Da de baja a un usuario del sistema
     public boolean baja(int id) throws RemoteException;
+    // Devuelve una lista con los clientes
+    public List<String> listarClientes() throws RemoteException;
+    // Devuelve una lista con los distribuidores
+    public List<String> listarDistribuidores() throws RemoteException;
 }
