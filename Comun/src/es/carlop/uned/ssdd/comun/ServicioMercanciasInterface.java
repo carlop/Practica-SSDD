449d65d24b30f01ef1fbc548058b63c5703f5ba5
Carlos López <yo@carlop.es>
2015-11-30 23:13:59 +0100

    Rellenada la interfaz del servicio de mercancias con los posibles métodos que utilizará.

--- Comun/src/es/carlop/uned/ssdd/comun/ServicioMercanciasInterface.java
+++ Comun/src/es/carlop/uned/ssdd/comun/ServicioMercanciasInterface.java
@@ -8,6 +8,21 @@
  */
 package es.carlop.uned.ssdd.comun;
 
-public interface ServicioMercanciasInterface {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
 
+public interface ServicioMercanciasInterface extends Remote {
+    // Introduce una demanda de una mercancía por el cliente
+    public void introducirDemanda(TipoMercancia tipoMercancia) throws RemoteException;
+    // Introduce una oferta de una mercancía por el distribuidor
+    public void introducirOferta(Oferta oferta) throws RemoteException;
+    // Elimina una oferta de una mercancía por el distribuidor
+    public void eliminarOferta(Oferta oferta) throws RemoteException;
+    // Devuelte una lista con las ofertas que recibe un cliente
+    public List<Oferta> recibirOfertas(int id) throws RemoteException;
+    // Devuelve una lista con todas las ofertas actuales
+    public List<Oferta> listarOfertas(TipoMercancia tipoMercancia) throws RemoteException;
+    // Devuelve una lista con todas las demandas actuales
+    public List<Integer> listarDemandas(TipoMercancia tipoMercancia) throws RemoteException;
 }
