cc7a6928ac83faf6a10c4b3e9dd350e43c3f1bb8
Carlos López <yo@carlop.es>
2015-12-01 19:08:02 +0100

    Añadido metodo para leer de la consola y limpiar la pantalla. Actualizado mostrarMenu para funcionar correctamente.

--- Comun/src/es/carlop/uned/ssdd/comun/InterfazGraficaUsuario.java
+++ Comun/src/es/carlop/uned/ssdd/comun/InterfazGraficaUsuario.java
@@ -8,11 +8,50 @@
  */
 package es.carlop.uned.ssdd.comun;
 
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class InterfazGraficaUsuario {
-    public static void mostrarMenu(String[] opciones) {
-	for (int i = 0; i < opciones.length; i++) {
-	   System.out.println((i+1) + ". " + opciones[i]); 
-	}
-	System.out.println(opciones.length + 1 + ". Salir");
+
+    private static Console console = System.console();
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static int mostrarMenu(String[] opciones) {
+        int opcion = 0;
+        System.out.println("Seleccione una opción:");
+        for (int i = 0; i < opciones.length; i++) {
+           System.out.println((i+1) + ". " + opciones[i]); 
+        }
+        System.out.println((opciones.length + 1) + ". Salir");
+        opcion = leerConsola();
+        return opcion;
+    }
+    
+    private static int leerConsola() {
+        if (console != null) {
+            return Integer.parseInt(console.readLine());
+        }
+        try {
+            return Integer.parseInt(reader.readLine());
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public static void limpiarPantalla() throws IOException {
+        try {
+            if (System.getProperty("os.name").startsWith("Window")) {
+                Runtime.getRuntime().exec("cls");
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (IOException e) {
+            for (int i = 0; i < 1000; i++) {
+                System.out.println();
+            }
+        }
     }
 }
