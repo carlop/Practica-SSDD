40b5e27d0d1d848f60484e37d8fd09a531109d83
Carlos López <yo@carlop.es>
2015-12-01 19:08:26 +0100

    Añadidas algunas opciones de menu a modo de prueba.

--- Cliente/src/es/carlop/uned/ssdd/cliente/Cliente.java
+++ Cliente/src/es/carlop/uned/ssdd/cliente/Cliente.java
@@ -7,12 +7,42 @@
  */
 package es.carlop.uned.ssdd.cliente;
 
+import java.io.IOException;
+
 import es.carlop.uned.ssdd.comun.InterfazGraficaUsuario;
 
 public class Cliente {
+    
+    private static int opcion = 0;
 
-    public static void main(String[] args) {
-	String[] opcionesMenu = {"Introducir demanda.", "Recibir ofertas.", "Comprar mercancía.", "Darse de baja en el sistema"};
-	InterfazGraficaUsuario.mostrarMenu(opcionesMenu);
+    public static void main(String[] args) throws IOException {
+        String[] opcionesMenuInicio = {"Introducir demanda.", "Recibir ofertas.", "Comprar mercancía.", "Darse de baja en el sistema"};
+        String[] opcionesMenu = {"Introducir demanda.", "Recibir ofertas.", "Comprar mercancía.", "Darse de baja en el sistema"};
+        InterfazGraficaUsuario.limpiarPantalla();
+        do {
+            opcion = InterfazGraficaUsuario.mostrarMenu(opcionesMenu);
+            switch (opcion) {
+            case 1:
+        	InterfazGraficaUsuario.limpiarPantalla();
+                System.out.println(opcion);
+                break;
+            case 2:
+        	InterfazGraficaUsuario.limpiarPantalla();
+                System.out.println(opcion);
+                break;
+            case 3:
+        	InterfazGraficaUsuario.limpiarPantalla();
+                System.out.println(opcion);
+                break;
+            case 4:
+        	InterfazGraficaUsuario.limpiarPantalla();
+                System.out.println(opcion);
+                break;
+            default:
+        	InterfazGraficaUsuario.limpiarPantalla();
+                break;
+            }
+        } while (opcion != 5);
+
     }
 }
