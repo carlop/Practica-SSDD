38744a7e55353d6dcc386ac88389002913a1cbaa
Carlos López <yo@carlop.es>
2015-12-01 21:07:36 +0100

    Añadido menu de opciones por defecto a modo de prueba

--- Regulador/src/es/carlop/uned/ssdd/regulador/Regulador.java
+++ Regulador/src/es/carlop/uned/ssdd/regulador/Regulador.java
@@ -8,10 +8,32 @@
  */
 package es.carlop.uned.ssdd.regulador;
 
+import java.io.IOException;
+
+import es.carlop.uned.ssdd.comun.InterfazGraficaUsuario;
+
 public class Regulador {
 
-    public static void main(String[] args) {
-        System.out.println("Regulador");
+    public static void main(String[] args) throws IOException {
+        int opcion = 0;
+        String[] opcionesMenu = {"Listar ofertas actuales.", "Listar demandas actuales.", "Listar clientes.",
+                "Listar distribuidores."};
+        InterfazGraficaUsuario.limpiarPantalla();
+        do {
+            opcion = InterfazGraficaUsuario.mostrarMenu(opcionesMenu);
+            switch (opcion) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+                break;
+            }
+        } while (opcion != 5);
     }
 
 }
