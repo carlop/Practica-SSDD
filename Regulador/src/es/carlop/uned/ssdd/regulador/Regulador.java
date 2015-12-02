3a14264f2ef08e2055486a790f939f9963ac0cdd
Carlos López <yo@carlop.es>
2015-12-02 23:13:50 +0100

    Servicio de autenticacion iniciado y añadido algunas opciones remotas a modo de ejemplo para comprobar que funcionan

--- Regulador/src/es/carlop/uned/ssdd/regulador/Regulador.java
+++ Regulador/src/es/carlop/uned/ssdd/regulador/Regulador.java
@@ -9,31 +9,92 @@
 package es.carlop.uned.ssdd.regulador;
 
 import java.io.IOException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.List;
 
 import es.carlop.uned.ssdd.comun.InterfazGraficaUsuario;
+import es.carlop.uned.ssdd.comun.ServicioAutenticacionInterface;
 
 public class Regulador {
+    
+    private static ServicioAutenticacionInterface servicioAutenticacion;
 
     public static void main(String[] args) throws IOException {
-        int opcion = 0;
-        String[] opcionesMenu = {"Listar ofertas actuales.", "Listar demandas actuales.", "Listar clientes.",
-                "Listar distribuidores."};
-        InterfazGraficaUsuario.limpiarPantalla();
-        do {
-            opcion = InterfazGraficaUsuario.mostrarMenu(opcionesMenu);
-            switch (opcion) {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            default:
-                break;
+        
+        // Gestor de seguridad
+        //System.setProperty("java.security.policy", "jaich");
+//        if (System.getSecurityManager() == null) {
+//            System.setSecurityManager(new SecurityManager());
+//        }
+
+        try {
+            servicioAutenticacion = new ServicioAutenticacionImpl();
+            Registry registry = LocateRegistry.createRegistry(9000);
+            registry.rebind("regulador", servicioAutenticacion);
+            System.out.println("Servicio de autenticacion preparado...");
+            int opcion = 0;
+            String[] opcionesMenu = {"Listar ofertas actuales.", "Listar demandas actuales.", "Listar clientes.",
+                    "Listar distribuidores."};
+            InterfazGraficaUsuario.limpiarPantalla();
+            do {
+                opcion = InterfazGraficaUsuario.mostrarMenu(opcionesMenu);
+                switch (opcion) {
+                case 1:
+            InterfazGraficaUsuario.limpiarPantalla();
+                    break;
+                case 2:
+            InterfazGraficaUsuario.limpiarPantalla();
+                    break;
+                case 3:
+                    InterfazGraficaUsuario.limpiarPantalla();
+                    listarClientes();
+                    break;
+                case 4:
+                    InterfazGraficaUsuario.limpiarPantalla();
+                    listarDistribuidores();
+                    break;
+                default:
+            InterfazGraficaUsuario.limpiarPantalla();
+                    break;
+                }
+            } while (opcion != 5);
+            System.out.println("Cerrando el regulador...");
+            System.exit(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Servicio de autenticacion no iniciado :(");
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+        
+    }
+
+    private static void listarClientes() {
+        List<String> listaClientes = new ArrayList<String>();
+        try {
+            listaClientes = servicioAutenticacion.listarClientes();
+            for (String cliente : listaClientes) {
+                System.out.println(cliente);
             }
-        } while (opcion != 5);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void listarDistribuidores() {
+        List<String> listaDistribuidores = new ArrayList<String>();
+        try {
+            listaDistribuidores = servicioAutenticacion.listarDistribuidores();
+            for (String distribuidor : listaDistribuidores) {
+                System.out.println(distribuidor);
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- Regulador/src/es/carlop/uned/ssdd/regulador/ServicioAutenticacionImpl.java
+++ Regulador/src/es/carlop/uned/ssdd/regulador/ServicioAutenticacionImpl.java
@@ -9,17 +9,43 @@
 package es.carlop.uned.ssdd.regulador;
 
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import es.carlop.uned.ssdd.comun.ServicioAutenticacionInterface;
 import es.carlop.uned.ssdd.comun.TipoUsuario;
 
-public class ServicioAutenticacionImpl implements ServicioAutenticacionInterface {
+public class ServicioAutenticacionImpl extends UnicastRemoteObject implements ServicioAutenticacionInterface {
+
+    private static final long serialVersionUID = 6086832664658177647L;
+
+    // Clientes registrados en el sistema
+    private Map<String, String> clientes = new HashMap<String, String>();
+
+    // Distribuidores registrados en el sistema
+    private Map<String, String> distribuidores = new HashMap<String, String>();
+    
+    // Lista de clientes conectados
+    private List<Integer> clientesConectados = new ArrayList<Integer>();
+    
+    // Lista de distribuidores conectados
+    private List<Integer> distribuidoresConectados = new ArrayList<Integer>();
+    
+    public ServicioAutenticacionImpl() throws RemoteException {
+        super();
+    }
 
     @Override
     public int registrar(String usuario, String password, TipoUsuario tipoUsuario) throws RemoteException {
-	// TODO Auto-generated method stub
-	return 0;
+        if (tipoUsuario == TipoUsuario.CLIENTE) {
+            clientes.put(usuario, password);
+        } else if (tipoUsuario == TipoUsuario.DISTRIBUIDOR) {
+            distribuidores.put(usuario, password);
+        }
+        return 0;
     }
 
     @Override
@@ -42,14 +68,28 @@ public class ServicioAutenticacionImpl implements ServicioAutenticacionInterface
 
     @Override
     public List<String> listarClientes() throws RemoteException {
-	// TODO Auto-generated method stub
-	return null;
+        List<String> listaClientes = new ArrayList<String>();
+        if (clientesConectados.size() > 0) {
+            for (Integer cliente : clientesConectados) {
+                listaClientes.add(Integer.toString(cliente));
+            }
+        } else {
+            listaClientes.add("No hay clientes conectados");
+        }
+        return listaClientes;
     }
 
     @Override
     public List<String> listarDistribuidores() throws RemoteException {
-	// TODO Auto-generated method stub
-	return null;
+        List<String> listaDistribuidores = new ArrayList<String>();
+        if (distribuidoresConectados.size() > 0) {
+            for (Integer cliente : clientesConectados) {
+                listaDistribuidores.add(Integer.toString(cliente));
+            }
+        } else {
+            listaDistribuidores.add("No hay distribuidores conectados");
+        }
+        return listaDistribuidores;
     }
 
 }
