84271cffb2f1ebe9e4f14b27183228b4f50fbaa4
Carlos López <yo@carlop.es>
2015-12-03 13:02:24 +0100

    Añadido codebase

---
+++ Comun/src/es/carlop/uned/ssdd/comun/Codebase.java
@@ -0,0 +1,19 @@
+package es.carlop.uned.ssdd.comun;
+
+public class Codebase {
+
+    public static final String CODEBASE = "java.rmi.server.codebase";
+    
+    public static void setCodeBase(Class<?> c) {
+        String ruta = c.getProtectionDomain().getCodeSource().getLocation().toString();
+        
+        String path = System.getProperty(CODEBASE);
+        
+        if (path != null && !path.isEmpty()) {
+            ruta = path + " " + ruta;
+        }
+        
+        System.setProperty(CODEBASE, ruta);
+    }
+
+}
